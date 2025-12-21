1ce10383f24cd05c29958a0ba6cc4d394eae77d8
shanku@boss_gnu <101804633+Shnku@users.noreply.github.com>
2025-12-21 23:25:01 +0530

    Resizing Id photo and make a4 printable grid  Changes to be committed: 	modified:   img_edit/demo_grid_a4.jpg 	modified:   img_edit/demo_resized.jpg 	modified:   img_edit/img_resize.py 	modified:   img_edit/imgrid_photo.py 	modified:   img_edit/print_canvas_size.py

--- img_edit/img_resize.py
+++ img_edit/img_resize.py
@@ -1,18 +1,69 @@
 from PIL import Image
+from print_canvas_size import (
+    getsize_oci_passport_35x35_mm,
+    getsize_pan_25x35_mm,
+    getsize_passport_2x2_inch,
+    getsize_stamp_20x25_mm,
+    getsize_visa_35x45_mm,
+)
 
 
-def resize_half(img):
-    print(img)
-    height, width = img.size
-    img = img.resize((int(height / 2), int(width / 2)))
-    print(img)
+def resize_equally_factor(img: Image.Image, resize_factor=0.5, rev=True):
+    print("original:", img)
+    width, height = img.size
+    if rev:
+        resize_factor = 1 - resize_factor
+    img = img.resize(
+        (int(width * resize_factor), int(height * resize_factor)),
+        Image.Resampling.LANCZOS,
+    )
+    print("resized:", img)
     return img
 
 
+def resize_with(img: Image.Image, template_size: tuple[float, float]):
+    to_width, to_height = template_size
+    scale_w = to_width / img.width
+    scale_h = to_height / img.height
+    print("scaling:", scale_w, scale_h)
+    scaling_factor = min(scale_w, scale_h)
+    img = resize_equally_factor(img, scaling_factor, rev=False)
+    return img
+
+
+def choose_size_options(PPI):
+    choice = int(
+        input("""   What to Convert? ...................
+        1) Indian Passport Photo: 2 x 2 inch.
+        2) Indian OCI (Overseas Citizenship of India): 3.5 x 3.5 cm.
+        3) Indian Visa Photo: 3.5 x 4.5 cm.
+        4) Indian PAN Card Photo: 2.5 x 3.5 cm.
+        5) Indian Stamp Size Photo: 2 x 2.5 cm.
+    Enter the format of your id photo:- """)
+    )
+    to_size = (0, 0)
+    match choice:
+        case 1:
+            to_size = getsize_passport_2x2_inch(PPI)
+        case 2:
+            to_size = getsize_oci_passport_35x35_mm(PPI)
+        case 3:
+            to_size = getsize_visa_35x45_mm(PPI)
+        case 4:
+            to_size = getsize_pan_25x35_mm(PPI)
+        case 5:
+            to_size = getsize_stamp_20x25_mm(PPI)
+    return to_size
+
+
 def main():
-    path = "./demo.jpg"
+    PPI = 300
+    path = "./demo_resized.jpg"
+
     with Image.open(path) as pic:
-        pic = resize_half(pic)
+        # pic = resize_equally_factor(pic)
+        pic = resize_with(pic, choose_size_options(PPI))
+        pic.show()
         pic.save("demo_resized.jpg")
 
 
--- img_edit/imgrid_photo.py
+++ img_edit/imgrid_photo.py
@@ -1,36 +1,61 @@
-from operator import index, indexOf
 from PIL import Image, ImageOps
-from img_resize import resize_half
-from print_canvas_size import create_a4_canvas
+from img_resize import resize_with
+from print_canvas_size import (
+    getsize_a4_canvas,
+    getsize_passport_2x2_inch,
+    getsize_visa_35x45_mm,
+    inches_to_pixels,
+)
 
 
-def create_grid(photo, no):
-    photo = ImageOps.expand(photo, 10, "white")
-    print(photo)
-    list_of_pics = [photo for _ in range(no)]
-    a4_size = create_a4_canvas(ppi=300)
+def border_photo(photo, border_inches=0.1, color="white"):
+    return ImageOps.expand(
+        photo,
+        inches_to_pixels(border_inches),
+        color,
+    )
+
+
+def create_grid(photos_list, margin=0.5, ppi=300):
+    print(photos_list[0])
+    a4_size = getsize_a4_canvas(ppi=ppi)
     canvas = Image.new("RGB", size=a4_size, color="white")
-
-    pos_x, pos_y = 0, 0
-    for pic in list_of_pics:
-        canvas.paste(pic, (int(pos_x), int(pos_y)))
-        pos_x += photo.width
-        if pos_x + photo.width >= a4_size[0]:
-            pos_x, pos_y = 0, pos_y + photo.height
-            if pos_y + photo.height >= a4_size[1]:
+    margin_start = inches_to_pixels(margin, ppi=ppi)
+    pos_x, pos_y = margin_start, margin_start
+    for pic in photos_list:
+        if pos_x + pic.width >= a4_size[0] - margin_start:
+            pos_x, pos_y = margin_start, pos_y + pic.height
+            if pos_y + pic.height >= a4_size[1] - margin_start:
                 print("less printed as page overflow")
                 break
-
+        canvas.paste(pic, (int(pos_x), int(pos_y)))
+        pos_x += pic.width
     canvas.show()
     return canvas
 
 
 def main():
     with Image.open("./demo_resized.jpg") as photo:
-        photo = resize_half(photo)
-        # photo.show()
+        photo = resize_with(
+            photo,
+            getsize_passport_2x2_inch(ppi=300),
+        )
+        photo2 = resize_with(
+            photo,
+            getsize_visa_35x45_mm(ppi=300),
+        )
+        photo = border_photo(photo)
+        photo2 = border_photo(photo2)
+        list_of_pics = (
+            [photo for _ in range(5)]
+            + [photo2 for _ in range(5)]
+            + [photo for _ in range(5)]
+        )
+        photo.show()
+        photo2.show()
         print(photo)
-        grid = create_grid(photo, no=30)
+        grid = create_grid(list_of_pics)
+        # grid.show()
         grid.save("demo_grid_a4.jpg")
 
 
